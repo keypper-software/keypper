289f286b6e11a665c3ce73e1a5b1c47cb8b0471b
Timi-Leyin <timileyinoyelekan11@gmail.com>
2025-04-26 13:38:18 +0100

    fix (cli): reset color back after changing color

--- apps/cli/src/utils/get-color.ts
+++ apps/cli/src/utils/get-color.ts
@@ -10,8 +10,8 @@ interface Params {
   style?: COLOR_STYLE_TYPE;
   text: any;
 }
-export default ({ color = "WHITE", style = "RESET", text }: Params):string => {
+export default ({ color = "WHITE", style = "RESET", text }: Params): string => {
   const colorStyle = COLOR_STYLE[style];
   const textColor = COLORS[color];
-  return `${colorStyle}${textColor}${text}`;
+  return `${colorStyle}${textColor}${text}${COLOR_STYLE.RESET}`;
 };
