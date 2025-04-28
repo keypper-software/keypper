131a8e3748470a5c4268da64476872da21cdc31d
Timi-Leyin <timileyinoyelekan11@gmail.com>
2025-04-28 23:24:11 +0100

    chore (cli): setup logout action

---
+++ apps/cli/src/actions/logout.ts
@@ -0,0 +1,12 @@
+import getColor from "../utils/get-color";
+
+export default async () => {
+  try {
+    console.log(
+      getColor({
+        text: "⚠️ You are already logged out",
+        color: "YELLOW",
+      })
+    );
+  } catch (error) {}
+};
