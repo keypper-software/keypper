6d3d403a7876bb518dc6d690e48f32a7bd07a900
Timi-Leyin <timileyinoyelekan11@gmail.com>
2025-04-30 11:39:30 +0100

    chore (cli): setup commands

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -5,6 +5,7 @@ import cli from "./cli";
 import login from "@/actions/login";
 import { VERSION } from "@/constants";
 import logout from "@/actions/logout";
+import selectWorkspace from "./actions/select-workspace";
 
 cli
   .name("keypper")
@@ -17,6 +18,28 @@ cli
   .command("login")
   .description("Login to manage your workspace")
   .action(login);
+
+cli
+  .command("init")
+  .description("Initialize Keypper configuration in your project");
+
+cli
+  .command("run")
+  .description("Run a command in the context of your Keypper workspace")
+  .argument("<command...>", "Command to run");
+
+cli
+  .command("workspace")
+  .option("-n, --name <name>", "Select a workspace By name")
+  .description(
+    "Manage your workspace, ignores the workspace defined in the config file"
+  )
+  .action(selectWorkspace);
+
+cli
+  .command("project")
+  .description("Select a project from a defined workspace")
+  .option("-n, --name <name>", "Select a project By name");
 
 cli
   .command("whoami")
